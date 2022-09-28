9d7dcb85fbab957c04ce39b7dbe95bede128c3f0
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-09-28 13:18:01 +0700

    Belajar php

---
+++ belajar-php/sintaks.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+        <?php 
+        echo "Hello world";// echo untuk mengeluarkan atau ouput ke browser,penting pakai titik koma biar tidak error untuk echo pennulisanya boleh huruf besar semua dan boleh juga kecil semua
+        echo "Hello world";// echo untuk mengeluarkan atau ouput ke browser
+        ?>
+</body>
+</html>
