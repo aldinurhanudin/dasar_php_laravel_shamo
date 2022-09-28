fad59600d7872abcea922889fe19471a99f42668
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-09-28 14:47:14 +0700

    looping

---
+++ belajar-php/looping-do-while/looping-do-while.php
@@ -0,0 +1,14 @@
+<?php 
+
+$a = 1;
+
+do{
+ echo "saya Bernilai". $a . "</br>";
+ $a++;
+}while ($a <= 10) ;
+
+/**notes
+ * perbedaaan while dengan do while
+ * kalau dowhile akan dijalankan dulu baru dicek 
+ * sedangakan while di cek dulu baru dijalankan
+ */
