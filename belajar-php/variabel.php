a1af3ee0b6594f24296032b0ca85c77b47e59bdd
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-09-28 13:37:18 +0700

    variabel

---
+++ belajar-php/variabel.php
@@ -0,0 +1,11 @@
+/** Notes aturan dalam php
+1.variable haru memperhatikan besar kecilnya huruf
+2.tidak boleh menuliskan yang aneh aneh seperti -
+3.dan hanya boleh huruf a-z
+ */
+<?php 
+$teks = "Halo dunia";
+$angka = 5;
+$angkadua = 10.5;//floatatau double
+
+echo $teks;//echo untuk mengeluarkan output dibrowseer
